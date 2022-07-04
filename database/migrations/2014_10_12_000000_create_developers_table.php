6a3f7c7214c2f8bcea8ba6242af800d3997d9250
Yukiman-byde <86790065+Yukiman-byde@users.noreply.github.com>
2022-07-04 20:38:21 +0900

    Revert "[add]マイグレーションファイルの作成"

--- database/migrations/2014_10_12_000000_create_developers_table.php
+++ database/migrations/2014_10_12_000000_create_developers_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
         	$table->timestamp('email_verified_at')->nullable();
         	$table->string('developer_avatar');
         	$table->string('password');
-        	$table->integer('age')->nullable();
+        	$table->integer('age')->nullable;
             $table->rememberToken();
             $table->timestamps();
         });
